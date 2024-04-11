7f044c3ec99d5c0b0db0e61f0b562b6dfd8f380f
Kamvalethu Mdanjelwa <ST10377628@rcconnect.edu.za>
2024-04-11 14:10:13 +0200

    Added option in scalingClass for user to choose from.

--- Final POE Part 1/scalingClass.cs
+++ Final POE Part 1/scalingClass.cs
@@ -8,7 +8,7 @@ namespace Final_POE_Part_1
 {
     internal class scalingClass
     {
-        addClass p = new addClass();
+        addClass p = new addClass(); //Creating object of addClass
 
 
         public void run()
@@ -19,8 +19,43 @@ namespace Final_POE_Part_1
         }
         public void adjust()  //Creating adjust method for selection of scaling
         {
-        }
-        public void half()   //Creating method for halving the arrays
+            while (true)
+            {
+                Console.WriteLine("Choose one of the following: \n1. Double all quantities \n2. Half all quantities \n3. Triple \n4. Clear all data");
+                int choice2 = Convert.ToInt32(Console.ReadLine());
+                if (choice2 == 1)
+                {
+                    double_();
+                    Console.WriteLine("Ingredients are Doubled!");
+                    Console.WriteLine();
+                    run();
+                    break;
+                }
+                else if (choice2 == 2)
+                {
+                    half();
+                    Console.WriteLine("Ingredients are Halved!");
+                    Console.WriteLine();
+                    run();
+                    break;
+                }
+                else if (choice2 == 3)
+                {
+                    triple();
+                    Console.WriteLine("Ingredients are Tripled!");
+                    Console.WriteLine();
+                    run();
+                    break;
+                }
+                else if (choice2 == 4)
+                {
+                    clear();
+                    run();
+                    break;
+                }
+
+            }
+            public void half()   //Creating method for halving the arrays
         {
         }
         public void double_()   //Creating method for doubling the arrays
