2ebb797688aea58d939d7a913b56e9363cf2f9bd
Kamvalethu Mdanjelwa <ST10377628@rcconnect.edu.za>
2024-05-12 15:52:07 +0200

    Added error handling and created new class recipeCount

--- Final POE Part 1/addClass.cs
+++ Final POE Part 1/addClass.cs
@@ -13,6 +13,7 @@ namespace Final_POE_Part_1
         public static String nameOfing; //Variable for name of Recipe
         public static int steps;        //Variable for steps
         public static int numOfIng;     //Variable for number of ingredients
+        
        
         private int choice;
 
@@ -21,43 +22,62 @@ namespace Final_POE_Part_1
         public static String[] ingArray;        //stores ingredints
         public static double[] amountArray;     //stores the amount of ingredients 
         public static String[] measureArray;    //stores the unit of measurement
-        public static String[] stepArray;       //stores the steps descriptions
-        
+        public static String[] stepArray;
+        public static double[] calorieTot;
+        public static String[] foodGroup;
+        public static List<String> ingL;
+        public static List<double> amountL;
+        public static List<String> measureL;
+        public static List<String> stepL;
+
         //Arrays to store the original values
         public static String[] ingArray2;
         public static double[] amountArray2;
         public static String[] measureArray2;
         public static String[] stepArray2;
+        public static List<String> ingL2;
+        public static List<double> amountL2;
+        public static List<String> measureL2;
+        public static List<String> stepL2;
 
 
         public void addRun() //Method that will run the recIng method to record recipe
         {
             addClass l = new addClass();  //Creates object
-            l.recIng();                    //Object is used to run recIng() method
+            l.recordRec();                    //Object is used to run recIng() method
 
         }
-        public void recIng() //method to run the recipe recording
+
+       
+        public void recordRec() //method to run the recipe recording
         {
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-
             Console.WriteLine("WELCOME TO RECIPE APP: ");  //Outputs message inside parameters
 
-            Console.WriteLine("Enter the recipe name");
-            nameOfing = Console.ReadLine();                 //Accepts user input and stores in the variable
+            //Console.WriteLine("Enter the recipe name");
+            //nameOfing = Console.ReadLine(); 
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+                //Accepts user input and stores in the variable
 
             Console.WriteLine("Enter number of ingredients ");
             numOfIng = int.Parse(Console.ReadLine());
-            
 
-            ingArray = new String[numOfIng];            //Declaring array size with numOfIng inputted by user
+            //List<String> ingL = new List<string>(numOfIng);
+            //List<double> amountL = new List<string>(numOfIng);
+            //List<String> measureL = new List<string>(numOfIng);
+            //List<String> stepL = new List<string>(numOfIng);
+            ingArray = new String[numOfIng];            //Declaring list size with numOfIng inputted by user
             amountArray = new double[numOfIng];
             measureArray = new String[numOfIng];
+            calorieTot=new double[numOfIng];
+            foodGroup= new String[numOfIng];
+
 
             ingArray2 = new String[numOfIng];            //Declaring original array size with numOfIng inputted by user
             amountArray2 = new double[numOfIng];
             measureArray2 = new String[numOfIng];
-
+            
 
 
             int ingNo = 0;                              //Created variable with 0 as value
@@ -69,17 +89,44 @@ namespace Final_POE_Part_1
                 ingArray2[i] += ingArray[i];
 
 
-                Console.WriteLine("Enter quantity of ingredient: ");
-                amountArray[i] = double.Parse(Console.ReadLine());
+                
+                do
+                {
+                    Console.WriteLine("Enter quantity of ingredient: ");
+                     
+                } while (!double.TryParse(Console.ReadLine(), out amountArray[i]));
                 amountArray2[i] += amountArray[i];
 
                 Console.WriteLine("Enter unit of measurement: ");
                 measureArray[i] = Console.ReadLine();
                 measureArray2[i] += measureArray[i];
+
+                
+                do
+                {
+                    Console.WriteLine("Enter calorie count: ");
+
+                } while (!double.TryParse(Console.ReadLine(), out calorieTot[i]));
+
+
+                Console.WriteLine("Enter food group: ");
+                foodGroup[i] = Console.ReadLine();
+
             }
 
-            Console.WriteLine("Enter the number of steps to be taken: ");
-            steps = Convert.ToInt32(Console.ReadLine());
+            double calLimit = calorieMeth(calorieTot);
+            Console.WriteLine(calLimit);
+            if(calLimit > 300)
+            {
+                Console.WriteLine("CALORY LIMIT EXCEEDED!!!");
+            }
+
+
+           
+            
+            do {
+                Console.WriteLine("Enter the number of steps to be taken: ");
+            } while (!int.TryParse(Console.ReadLine(), out steps));
 
 
             stepArray = new string[steps];
@@ -106,7 +153,7 @@ namespace Final_POE_Part_1
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 1)                        //responds to user choice by running statement if matched with user input
                 {
-                    recIng();
+                    recordRec();
                     break;
                 }
                 else if (choice == 2)
@@ -134,6 +181,16 @@ namespace Final_POE_Part_1
             
 
         }
+
+        public double calorieMeth(double[] calorieTot)
+        {
+            double result = 0;
+            for(int i = 0; i < calorieTot.Length; i++)
+            {
+                result+= calorieTot[i];
+            }
+            return result;
+        }
         public void viewRec()                   //Method for viewing the recipe
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -153,8 +210,51 @@ namespace Final_POE_Part_1
             Console.WriteLine();
             choices();
         }
+        public void menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please choose one of the following: ");
+                Console.WriteLine("1. Create Recipe ");
+                Console.WriteLine("2. Search Recipe ");
+                Console.WriteLine("3. Display Recipe ");
+                Console.WriteLine("4. Exit Application ");
+                String ch=Console.ReadLine();
+                switch (ch)
+                {
+                    
+                        
+                }
+            }
+        }
     
-    
+    }
+    class recipeCount
+    {
+        private Dictionary<String, addClass> recDictionary;     //Declaring the Dictionary for the class
+        public recipeCount(Dictionary<String, addClass> recDictionary)  //assigning the dictionary to the recipeCount class
+        {
+            this.recDictionary = recDictionary;
+        }
+
+
+        public void logRecipe()
+        {
+            Console.WriteLine("Enter number or recipes you wish to record: ");
+            int recipeNum;
+            if (!int.TryParse(Console.ReadLine(), out recipeNum))
+            {
+                for (int i = 0; i < recipeNum; i++) {
+                    Console.WriteLine("Enter Recipe Name: ");
+                    String recName = Console.ReadLine();
+                    addClass recipe = new addClass();
+                    recipe.recordRec();
+                    recDictionary.Add(recName, recipe);
+
+                }
+            }
+
+        }
     }
 
 }
